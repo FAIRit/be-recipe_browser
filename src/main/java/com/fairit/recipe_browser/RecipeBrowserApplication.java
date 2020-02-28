2025d96c45d3e490de90b644e08be63e34953de1
marlena <marlena.karpiej@gmail.com>
2020-02-28 23:05:49 +0100

    initial commit

---
+++ src/main/java/com/fairit/recipe_browser/RecipeBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.fairit.recipe_browser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBrowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fairit/recipe_browser/RecipeBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fairit.recipe_browser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
