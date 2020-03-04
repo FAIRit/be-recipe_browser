232928ddf00fa3d1ac12d61bed4561b89274b052
marlena <marlena.karpiej@gmail.com>
2020-03-04 22:32:03 +0100

    created registration and login views

---
+++ src/main/java/com/fairit/recipe_browser/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.fairit.recipe_browser.controller;
+
+import com.fairit.recipe_browser.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/register")
+    public String getRegisterForm() {
+        return "user/userRegister";
+    }
+
+    @PostMapping("/register")
+    public String submitRegisterForm(@RequestParam(name = "username") String username,
+                                     @RequestParam(name = "password") String password,
+                                     @RequestParam(name = "password-confirm") String passwordConfirm) {
+        userService.registerUser(username, password, passwordConfirm);
+
+        return "redirect:/login";
+    }
+}
