fc14f4408d9a54ee0c2ed4cf6ab046a054c7e1b2
marlena <marlena.karpiej@gmail.com>
2020-03-05 09:07:42 +0100

    created registration and login views

--- src/main/java/com/fairit/recipe_browser/configuration/DataInitializer.java
+++ src/main/java/com/fairit/recipe_browser/configuration/DataInitializer.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
+@Component
 public class DataInitializer implements ApplicationListener<ContextRefreshedEvent> {
 
     @Autowired
