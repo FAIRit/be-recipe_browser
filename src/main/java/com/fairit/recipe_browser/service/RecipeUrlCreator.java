f22ffe5ea58e612b4183f2bb6b64cbd62b371164
marlena <marlena.karpiej@gmail.com>
2020-03-10 22:19:40 +0100

    created controller findRecipeByIngredients

--- src/main/java/com/fairit/recipe_browser/service/RecipeUrlCreator.java
+++ src/main/java/com/fairit/recipe_browser/service/RecipeUrlCreator.java
@@ -1,6 +1,5 @@
 package com.fairit.recipe_browser.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +8,7 @@ public class RecipeUrlCreator {
     @Value("${myDefaultApiKey}")
     private String key;
 
-    public String createURLWithKey(String url){
+    public String createURLWithKey(String url) {
         return url + "&apiKey=" + key;
     }
 }
