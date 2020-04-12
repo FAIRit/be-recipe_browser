0d387c1dd7dad8b1edacf56628717b2b2bb02159
marlena <marlena.karpiej@gmail.com>
2020-04-12 14:32:59 +0200

    create RecipeRandomResponse

---
+++ src/main/java/com/fairit/recipe_browser/model/random/recipe/RecipeRandomResponse.java
@@ -0,0 +1,15 @@
+package com.fairit.recipe_browser.model.random.recipe;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecipeRandomResponse {
+
+    private List<RecipeRandom> recipes;
+}
